b54b44b09deff3855629fccfa20701b16e07755a
Natsuko Higashikuni <nokusukun@yahoo.co.jp>
2020-11-15 06:25:24 +0800

    feat(misc): Updated additional files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # genkai-client-js
+Javascript client for https://github.com/genkai-net/genkai-server-go
+
+Usage
+```javascript
+import {GenkaiClient, GenkaiClientJSON} from "genkai-client";
+
+let app = GenkaiClient("http://localhost:9302");
+let response = await app.join("hello", "world");
+console.log(response);
+```
+
+Returns
+```javascript
+"hello:world"
+```
+
+See: https://github.com/genkai-net/genkai-server-go for more documentation and samples.
--- index.js
+++ index.js
@@ -1,63 +1,106 @@
-let makeSession = ()=>{
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  }
+let makeSession = () => {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
 ;
 
-let getHandler = (obj,prop)=>{
-    return async(...params)=>{
-      let r = await fetch(`${obj.remote}/${obj.entrypoint}`, {
-        method: "post",
-        body: JSON.stringify({
-          fn: prop,
-          p: params
-        }),
-        headers: {
-          "genkai-session": obj.session
+let getHandler = (obj, prop) => {
+    return async (...params) => {
+        if (!!obj[prop]) {
+            return obj[prop]
         }
-      });
 
-      let response = await r.json();
-      if (!!response.e) {
-        throw Error(response.e)
-      }
+        let debug = "";
+        if (!!obj.debug) {
+            debug = `?p=${params}&fn=${prop}`
+        }
 
-      if (!!response.r && response.r.length === 1) {
-        return response.r[0]
-      }
+        let r = await fetch(`${obj.remote}/${obj.entrypoint}${debug}`, {
+            method: "post",
+            body: JSON.stringify({
+                fn: prop,
+                p: params,
+            }),
+            headers: {
+                "genkai-session": obj.session
+            }
+        });
 
-      return response.r
+        let response = await r.json();
+        if (!!response.e) {
+            throw Error(response.e)
+        }
+
+        if (!!response.r && response.r.length === 1) {
+            return response.r[0]
+        }
+
+        return response.r
     }
-  }
-;
-
-let GenkaiClient = (remote, entrypoint)=>{
-  if (!entrypoint) {
-    entrypoint = "__genkai_endpoint"
-  }
-
-  let session = entrypoint + makeSession();
-  if (typeof sessionStorage !== "undefined") {
-    session = sessionStorage.getItem("genkai-session") || session;
-    sessionStorage.setItem("genkai-session", session);
-  }
-
-  return new Proxy({
-    remote,
-    entrypoint,
-    session,
-    newSession () {
-      sessionStorage.removeItem("genkai-session");
-      this.session = entrypoint + makeSession();
-      if (typeof sessionStorage !== "undefined") {
-        sessionStorage.setItem("genkai-session", session);
-      }
-    }
-  },{
-    get: getHandler
-  })
 };
+
+let getHandlerJSON = (obj, prop) => {
+    return async (param) => {
+        let debug = "";
+        if (!!obj.debug) {
+            debug = `?p=${params}&fn=${prop}`
+        }
+
+        let r = await fetch(`${obj.remote}/${obj.entrypoint}${debug}`, {
+            method: "post",
+            body: JSON.stringify({
+                fn: prop,
+                json: JSON.stringify(param)
+            }),
+            headers: {
+                "genkai-session": obj.session
+            }
+        });
+
+        let response = await r.json();
+        if (!!response.e) {
+            throw Error(response.e)
+        }
+
+        if (!!response.r && response.r.length === 1) {
+            return response.r[0]
+        }
+
+        return response.r
+    }
+};
+
+let GenkaiClient = (handler) => {
+    return (remote, entrypoint, debug) => {
+        if (!entrypoint) {
+            entrypoint = "__genkai_endpoint"
+        }
+
+        let session = entrypoint + makeSession();
+        if (typeof sessionStorage !== "undefined") {
+            session = sessionStorage.getItem("genkai-session") || session;
+            sessionStorage.setItem("genkai-session", session);
+        }
+
+        return new Proxy({
+            debug,
+            remote,
+            entrypoint,
+            session,
+            newSession() {
+                sessionStorage.removeItem("genkai-session");
+                this.session = entrypoint + makeSession();
+                if (typeof sessionStorage !== "undefined") {
+                    sessionStorage.setItem("genkai-session", session);
+                }
+            }
+        }, {
+            get: handler
+        })
+    };
+}
 
 
 module.exports = {
-    GenkaiClient
+    GenkaiClient: GenkaiClient(getHandler),
+    GenkaiClientJSON: GenkaiClient(getHandlerJSON)
 };
